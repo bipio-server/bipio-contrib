b2120caad9fc720e3abebf6d66c86e59d2344b73
Michael Pearson <github@x0rz.com>
2015-05-19 11:59:44 -0400

    added /rpc/stats/bips/running endpoint

--- modules/stats/index.js
+++ modules/stats/index.js
@@ -19,11 +19,11 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-var Q = require('q'),
-  moment = require('moment'),
-  _ = require('underscore');
+ var Q = require('q'),
+ moment = require('moment'),
+ _ = require('underscore');
 
-function StatModule(options) {
+ function StatModule(options) {
   this.options = options;
 }
 
@@ -33,18 +33,18 @@ function nowUTCMSeconds() {
 
 StatModule.prototype = {
   actions : function(next) {
-  	actionsList = [];
+    actionsList = [];
     this.dao.findFilter(
-	  'channels',
-	  {},
-	  function(err, results) {
-	    if (err) {
-		 next(err);
-		} else {
-		  next(false, results);
-		}
-	  }
-	);
+      'channels',
+      {},
+      function(err, results) {
+        if (err) {
+         next(err);
+       } else {
+        next(false, results);
+      }
+    }
+    );
   },
   users : function(next) {
     this.dao.findFilter(
@@ -57,12 +57,12 @@ StatModule.prototype = {
           next(false, results.length);
         }
       }
-    );
+      );
   },
   returningUsers : function(next) {
     var self = this,
-      now = Math.floor(nowUTCMSeconds() / 1000),
-      then = now - (60 * 60 * 24);
+    now = Math.floor(nowUTCMSeconds() / 1000),
+    then = now - (60 * 60 * 24);
 
     this.dao.findFilter(
       'account',
@@ -86,33 +86,33 @@ StatModule.prototype = {
             })
         }
       }
-    );
+      );
   },
 
   leadUsers : function(next) {
     var self = this,
-      leaderboard = [];
+    leaderboard = [];
 
     this.dao.aggregate(
       'bip',
       [
-        {
-          $group : {
-            '_id' : "$owner_id",
-            'bips' : {
-              $push : {
-                name : "$name",
-                type : "$type",
-                paused : "$paused",
-                _last_run : "$_last_run",
-                _channel_idx : "$_channel_idx"
-              }
-            },
-            'count' : {
-              $sum : 1
+      {
+        $group : {
+          '_id' : "$owner_id",
+          'bips' : {
+            $push : {
+              name : "$name",
+              type : "$type",
+              paused : "$paused",
+              _last_run : "$_last_run",
+              _channel_idx : "$_channel_idx"
             }
+          },
+          'count' : {
+            $sum : 1
           }
         }
+      }
       ],
       function(err, results) {
         if (err) {
@@ -167,7 +167,7 @@ StatModule.prototype = {
                       });
                     }
                   }
-                );
+                  );
               } else {
                 // get account
                 self.dao.find('account', { id : aggResults[idx]._id }, function(err, result) {
@@ -194,234 +194,263 @@ StatModule.prototype = {
             })(results, i);
           }
         }
-      }
-    );
-  },
-  recentUsers : function(next, fromUnix, toUnix) {
-    var self = this,
+      });
+    },
+    recentUsers : function(next, fromUnix, toUnix) {
+      var self = this,
       now = Math.floor(nowUTCMSeconds() / 1000),
       then = now - (60 * 60 * 24),
       filter = {};
 
-    if (undefined === fromUnix && undefined === toUnix) {
-      filter.created = {
-        '$gt' : then
-      };
-    } else {
-      //
-      if (fromUnix  && fromUnix > 0) {
+      if (undefined === fromUnix && undefined === toUnix) {
         filter.created = {
-          '$gt' : fromUnix
+          '$gt' : then
         };
-      }
+      } else {
+          //
+          if (fromUnix  && fromUnix > 0) {
+            filter.created = {
+              '$gt' : fromUnix
+            };
+          }
 
-      //
-      if (toUnix  && toUnix > 0) {
-        if (!filter.created) {
-          filter.created = {};
-        }
-
-        filter.created['$lt'] = toUnix;
-      }
-    }
-
-    this.dao.findFilter(
-      'account',
-      filter,
-      function(err, results) {
-        var defer, promises = [];
-
-        if (err) {
-          next(err);
-        } else {
-
-          if (results.length) {
-            for (var i = 0; i < results.length; i++) {
-              defer = Q.defer();
-              promises.push(defer.promise);
-
-              (function(account, defer) {
-                self.dao.list('bip', undefined, 1, 1, [ 'created', 'asc' ], { owner_id : account.id }, function(err, modelName, results) {
-                  var struct = {
-                    account : {
-                      id : account.id,
-                      name : account.name,
-                      username : account.username,
-                      email_account : account.email_account
-                    },
-                    total : 0,
-                    acct_diff_seconds : 0,
-                    created_day : moment(account.created).format('YYYYMMDD'),
-                    ttfb : 0 // time to first bip (seconds)
-                  };
-
-                  if (err) {
-                    defer.reject(err);
-                  } else {
-                    struct.total = results.total;
-                    struct.acct_diff_seconds = now - account.created;
-
-                    if (results.data && results.data.length) {
-                      struct.ttfb = Math.floor( (results.data[0].created / 1000) - account.created);
-                    }
-                    defer.resolve(struct);
-                  }
-                });
-              })(results[i], defer);
+          //
+          if (toUnix  && toUnix > 0) {
+            if (!filter.created) {
+              filter.created = {};
             }
 
-            Q.all(promises).then(
-              function(structs) {
-                var
+            filter.created['$lt'] = toUnix;
+          }
+        }
+
+        this.dao.findFilter(
+          'account',
+          filter,
+          function(err, results) {
+            var defer, promises = [];
+
+            if (err) {
+              next(err);
+            } else {
+
+              if (results.length) {
+                for (var i = 0; i < results.length; i++) {
+                  defer = Q.defer();
+                  promises.push(defer.promise);
+
+                  (function(account, defer) {
+                    self.dao.list('bip', undefined, 1, 1, [ 'created', 'asc' ], { owner_id : account.id }, function(err, modelName, results) {
+                      var struct = {
+                        account : {
+                          id : account.id,
+                          name : account.name,
+                          username : account.username,
+                          email_account : account.email_account
+                        },
+                        total : 0,
+                        acct_diff_seconds : 0,
+                        created_day : moment(account.created).format('YYYYMMDD'),
+                        ttfb : 0 // time to first bip (seconds)
+                      };
+
+                      if (err) {
+                        defer.reject(err);
+                      } else {
+                        struct.total = results.total;
+                        struct.acct_diff_seconds = now - account.created;
+
+                        if (results.data && results.data.length) {
+                          struct.ttfb = Math.floor( (results.data[0].created / 1000) - account.created);
+                        }
+                        defer.resolve(struct);
+                      }
+                    });
+                })(results[i], defer);
+              }
+
+              Q.all(promises).then(
+                function(structs) {
+                  var
                   ttfbTotal = 0
                   ttfbDivisor = 0;
 
-                for (var i = 0; i < structs.length; i++) {
-                  if (structs[i].ttfb) {
-                    ttfbTotal += structs[i].ttfb;
-                    ttfbDivisor++;
+                  for (var i = 0; i < structs.length; i++) {
+                    if (structs[i].ttfb) {
+                      ttfbTotal += structs[i].ttfb;
+                      ttfbDivisor++;
+                    }
                   }
-                }
 
-                next(
-                  false,
-                  {
-                    now : now,
-                    since : then,
-                    count : structs.length,
-                    ttfb_avg : ttfbDivisor ? ttfbTotal / ttfbDivisor : 0,
-                    ttfb_pct : structs.length ? ttfbDivisor / structs.length : 0,
-                    stat : structs
-                  }
-                );
-              },
-              function(err) {
-                next(err);
-              }
-            );
-          } else {
-            next(err, {});
+                  next(
+                    false,
+                    {
+                      now : now,
+                      since : then,
+                      count : structs.length,
+                      ttfb_avg : ttfbDivisor ? ttfbTotal / ttfbDivisor : 0,
+                      ttfb_pct : structs.length ? ttfbDivisor / structs.length : 0,
+                      stat : structs
+                    }
+                  );
+                },
+                function(err) {
+                  next(err);
+                }
+              );
+            } else {
+              next(err, {});
+            }
           }
         }
-      }
-    );
-  },
-  bips : function(next) {
-    this.dao.findFilter(
-      'bip',
-      {},
-      function(err, results) {
-        if (err) {
-          next(err);
-        } else {
-          next(false, results.length);
+      );
+    },
+    bips : function(next) {
+      this.dao.findFilter(
+        'bip',
+        {},
+        function(err, results) {
+          if (err) {
+            next(err);
+          } else {
+            next(false, results.length);
+          }
         }
-      }
-    );
-  },
-  // @todo - created timestamp resolution mismatch? ms or seconds?
-  recentBips : function(next, fromUnix, toUnix) {
-    var self = this,
+        );
+    },
+    // @todo - created timestamp resolution mismatch? ms or seconds?
+    recentBips : function(next, fromUnix, toUnix) {
+      var self = this,
       now = nowUTCMSeconds(),
       then = now - (60 * 60 * 24 * 1000),
       filter = {},
       dayGroup = false;
 
-    if (undefined === fromUnix && undefined === toUnix) {
-      filter.created = {
-        '$gt' : then
-      };
-    } else {
-      //
-      if (fromUnix  && fromUnix > 0) {
+      if (undefined === fromUnix && undefined === toUnix) {
         filter.created = {
-          '$gt' : fromUnix
+          '$gt' : then
         };
-      }
-
-      //
-      if (toUnix  && toUnix > 0) {
-        if (!filter.created) {
-          filter.created = {};
+      } else {
+        //
+        if (fromUnix  && fromUnix > 0) {
+          filter.created = {
+            '$gt' : fromUnix
+          };
         }
 
-        filter.created['$lt'] = toUnix;
-      }
-      dayGroup = true;
-    }
+        //
+        if (toUnix  && toUnix > 0) {
+          if (!filter.created) {
+            filter.created = {};
+          }
 
-    this.dao.findFilter(
-      'bip',
-      filter,
-      function(err, results) {
-        if (err) {
-          next(err);
-        } else {
-          var dayResults = {}, bipDate;
-          if (dayGroup) {
-            for (var i = 0; i < results.length; i++) {
-              bipDate = moment(results[i].created).format('YYYYMMDD');
-              if (!dayResults[bipDate]) {
-                dayResults[bipDate] = 0;
-              }
-              dayResults[bipDate]++;
-            }
-            next(false, dayResults);
+          filter.created['$lt'] = toUnix;
+        }
+        dayGroup = true;
+      }
+
+      this.dao.findFilter(
+        'bip',
+        filter,
+        function(err, results) {
+          if (err) {
+            next(err);
           } else {
-            next(false, results.length);
+            var dayResults = {}, bipDate;
+            if (dayGroup) {
+              for (var i = 0; i < results.length; i++) {
+                bipDate = moment(results[i].created).format('YYYYMMDD');
+                if (!dayResults[bipDate]) {
+                  dayResults[bipDate] = 0;
+                }
+                dayResults[bipDate]++;
+              }
+              next(false, dayResults);
+            } else {
+              next(false, results.length);
+            }
           }
         }
-      }
-    );
-  },
-  setDAO : function(dao) {
-    this.dao = dao;
-  },
-  _respond : function(stat, res) {
-    return function(err, result) {
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        var resp = {};
-        resp[stat] = result;
-        res.status(200).send(resp);
-      }
-    }
-  },
-  _notFound : function(res) {
-    res.status(404).end();
-  },
-  routes : function(app, authWrapper) {
-    var self = this;
-    app.get('/rpc/stats/:stat/:mode?', authWrapper, function(req, res) {
-      if (req.user && req.user.user.is_admin && req.params.stat) {
-        switch (req.params.stat) {
-          case 'users' :
-            if (req.params.mode) {
-              // recent signup stats
-              if ('recent' === req.params.mode) {
-                self.recentUsers(self._respond(req.params.stat, res));
-
-              } else if ('all' === req.params.mode) {
-                self.recentUsers(self._respond(req.params.stat, res), req.query.fromUnix || 0, req.query.toUnix || 0);
-
-              // returning users stats
-              } else if ('returning' === req.params.mode) {
-                self.returningUsers(self._respond(req.params.stat, res));
-
-              } else if ('leaderboard' === req.params.mode) {
-                self.leadUsers(self._respond(req.params.stat, res));
-
-              } else {
-                self._notFound(res);
+        );
+    },
+    runningBips : function(next) {
+      var self = this,
+        filter = [
+          {
+            $group : {
+              '_id' : "$day",
+              'bips_total' : {
+                $sum : "$bips_total"
+              },
+              'share_total' : {
+                $sum : "$share_total"
+              },
+              'channels_total' : {
+                $sum : "$channels_total"
               }
-            } else {
-              self.users(self._respond(req.params.stat, res));
             }
-            break;
-		  case 'actions' :
-		    self.actions(self._respond(req.params.stat, res));
-			break;
+          }
+        ];
+
+      this.dao.aggregate(
+        'stats_account',
+        filter,
+        function(err, results) {
+          next(err, results);
+        }
+      );
+    },
+    setDAO : function(dao) {
+      this.dao = dao;
+    },
+    _respond : function(stat, res) {
+      return function(err, result) {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          var resp = {};
+          resp[stat] = result;
+          res.status(200).send(resp);
+        }
+      }
+    },
+    _notFound : function(res) {
+      res.status(404).end();
+    },
+    routes : function(app, authWrapper) {
+      var self = this;
+      app.get('/rpc/stats/:stat/:mode?', authWrapper, function(req, res) {
+        if (req.user && req.user.user.is_admin && req.params.stat) {
+          switch (req.params.stat) {
+
+            case 'users' :
+              if (req.params.mode) {
+                  // recent signup stats
+                  if ('recent' === req.params.mode) {
+                    self.recentUsers(self._respond(req.params.stat, res));
+
+                  } else if ('all' === req.params.mode) {
+                    self.recentUsers(self._respond(req.params.stat, res), req.query.fromUnix || 0, req.query.toUnix || 0);
+
+                  // returning users stats
+                } else if ('returning' === req.params.mode) {
+                  self.returningUsers(self._respond(req.params.stat, res));
+
+                } else if ('leaderboard' === req.params.mode) {
+                  self.leadUsers(self._respond(req.params.stat, res));
+
+                } else {
+                  self._notFound(res);
+                }
+              } else {
+                self.users(self._respond(req.params.stat, res));
+              }
+              break;
+
+           case 'actions' :
+              self.actions(self._respond(req.params.stat, res));
+              break;
+
           case 'bips' :
             if (req.params.mode) {
               if ('recent' === req.params.mode) {
@@ -429,6 +458,9 @@ StatModule.prototype = {
 
               } else if ('all' === req.params.mode) {
                 self.recentBips(self._respond(req.params.stat, res), req.query.fromUnix || 0, req.query.toUnix || 0);
+
+              } else if ('running' === req.params.mode) {
+                self.runningBips(self._respond(req.params.stat, res), req.query.fromDate || 0, req.query.toDate || 0);
 
               } else {
                 self._notFound(res);
@@ -439,11 +471,11 @@ StatModule.prototype = {
             break;
           default :
             self._notFound(res);
+          }
+        } else {
+          res.status(403).end();
         }
-      } else {
-        res.status(403).end();
-      }
-    });
+      });
   }
 };
 
